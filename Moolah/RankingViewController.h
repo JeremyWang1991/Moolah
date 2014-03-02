50fd99c306e2d751eddb75c562c7b89f4d367c9b
Michael <michaelmfok@gmail.com>
2014-03-02 01:58:09 -0500

    Added Census view (commented out)

--- Moolah/RankingViewController.h
+++ Moolah/RankingViewController.h
@@ -11,11 +11,18 @@
 #import "Ranking.h"
 
 
-@interface RankingViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, Ranking>
+@interface RankingViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, Ranking, UIPickerViewDelegate, UIPickerViewDataSource>
 
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) NSArray *tweetsArray;
 @property (strong, nonatomic) NSMutableDictionary *votesDictionary;
 - (IBAction)submitPressed:(id)sender;
+
+//UI
+@property (strong, nonatomic) UIView * bgOverlay;
+@property (strong, nonatomic) UIView * censusView;
+
+@property (strong, nonatomic) UILabel * gender;
+@property (strong, nonatomic) UIPickerView * picker;
 
 @end
